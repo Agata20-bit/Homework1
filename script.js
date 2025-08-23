c44578efff359dc084ca7236208a70751e5e16ff
Agata <agat.2012@yandex.ru>
2025-08-23 13:16:43 +0500

    added game 1

--- script.js
+++ script.js
@@ -1,50 +1,28 @@
-//task1
-    let a = 20;
-    alert (a);
-//task2
-    let b = 2007; 
-    alert(b); 
-//task3
-    const creatorName = "Brendan Eich"; 
-    alert (creatorName);
-//task4
-    let x = 10;
-    let y = 2;
-    let sum = x + y;
-    let difference = x - y;
-    let product = x * y;
-    let quotient = x / y; 
-    alert(sum);
-    alert(difference);
-    alert(product);
-    alert(quotient);
-//task5
-    let result = 2 ** 5;;
-    alert(result);
-//task6
-    let c = 9;
-    let d = 2;
-    let remainder = c % d;
-    alert(remainder);
-//task7
-    let num = 1;
-    num = num + 5;
-    num = num - 3;
-    num = num * 7;
-    num = num / 3;
-    num = num + 1;
-    num = num - 1;
-    alert(num);
-//task8
-    let age = prompt("Сколько вам лет?");
-    alert(age);
-//task9
-    const user = {
-   name: 'Агата',
-   age: 38,
-   isAdmin: true  
-    };
-    console.log(user);
-//task10
-    const userName = prompt('Как вас зовут?');
-    alert(`Привет, ${userName}`);
+function startGame1() {
+    let targetNumber = Math.floor(Math.random() * 100) + 1; // Исправлено match на Math
+    let attempts = 0; // Добавляем счетчик попыток
+    
+    while (true) {
+        let guess = prompt('Угадай число от 1 до 100.');
+        guess = Number(guess);
+        
+        if (isNaN(guess) || guess < 1 || guess > 100) {
+            alert('Пожалуйста, введите число от 1 до 100.');
+            continue;
+        }
+        
+        attempts++;
+        
+        if (guess === targetNumber) {
+            alert(`Поздравляю, ты угадал число за ${attempts} попыток!`);
+            break;
+        } 
+        else if (guess < targetNumber) {
+            alert('Больше! Попробуй еще раз');
+        } 
+        else {
+            alert('Меньше! Попробуй еще раз');
+        }
+    }
+    console.log('Кнопка "Играть" нажата!');
+}

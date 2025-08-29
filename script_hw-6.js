cea8849e5385d209230e5deee02e84aab358edca
Agata <agat.2012@yandex.ru>
2025-08-29 22:21:47 +0500

    29.08.25

---
+++ script_hw-6.js
@@ -0,0 +1,38 @@
+//task1
+// const array = [1, 5, 4, 10, 0, 3];
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 10) {
+//         console.log(array[i]);
+//         break;
+//     }
+//     console.log(array[i]);
+// }
+
+//task2
+// const numbs = [1, 5, 4, 10, 0, 3];
+// console.log(numbs[2]);
+
+//task3
+// let number = [1, 3, 5, 10, 20];
+// let сombinedNumber = number.join(" ");
+// console.log(сombinedNumber); 
+
+//task4
+// const arr = [];
+
+// for (let i = 0; i < 3; i++) {
+//     const arr2 = [];
+//    for (let i = 0; i < 3; i++) {
+//         arr2.push(1);
+//    }
+//     arr.push(arr2);   
+// }
+
+// console.log (arr);
+
+//task5
+const array = 1, 1, 1;
+array.push(2, 2, 2);
+
+console.log(array);

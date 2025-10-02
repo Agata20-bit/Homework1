76cd6452216809d0bab7f70741957953357363bf
Agata <agat.2012@yandex.ru>
2025-10-02 21:21:01 +0500

    задание 1

---
+++ script_hw-9.js
@@ -0,0 +1,18 @@
+//task1
+    const btnEl = document.querySelector('.btn');
+        const content = document.querySelector('.content');
+    
+        btnEl.addEventListener('click', () => {
+            const headingEl = document.createElement ('h2');
+            headingEl.textContent = 'Задание 1';
+            headingEl.classList.add('heading');
+            contentEl.appenChild(headingEl);      
+        
+        })
+
+
+
+
+
+
+
